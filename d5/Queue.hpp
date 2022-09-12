f42036cd208e65c71464c22b80843b52a058be21
zjk <2455650395@qq.com>
2022-09-12 17:21:16 +0800

    第五次作业 0.02

--- d5/Queue.hpp
+++ d5/Queue.hpp
@@ -17,30 +17,30 @@ namespace MercedesKK
 	{
 	public:
 		using value_type = T;
-		static const int default_size = 10;
+		static const int _default_size = 10;
 	public:
-		Queue() :begin(0), end(0), capacity(default_size) { data = new value_type[default_size]; };
-		~Queue() { delete[]data; };
+		Queue() :begin(0), _end(0), _capacity(_default_size) { _data = new value_type[_default_size]; };
+		~Queue() { delete[]_data; };
 	private:
-		value_type* data;	///< 元素顺序容器
+		value_type* _data;	///< 元素顺序容器
 		int begin;			///< 头指针（同迭代器）
-		int end;			///< 尾指针
-		size_t capacity;	///< 容器的容量   （size的求法由函数实现，不再单独设置size数据）
+		int _end;			///< 尾指针
+		size_t _capacity;	///< 容器的容量   （size的求法由函数实现，不再单独设置size数据）
 
 	public:
 		void push(const value_type& e);
 		void pop();
 
-		value_type& front() const	{ return data[begin]; };
-		value_type& back() const	{ return data[end - 1]; };
+		value_type& front() const	{ return _data[begin]; };
+		value_type& back() const	{ return _data[_end - 1]; };
 
 
 		/// @attention size指的是容器元素数量个数
-		size_t size() const			{ return end - begin; };
+		size_t size() const			{ return _end - begin; };
 
-		bool empty() const			{ return begin == end; };
+		bool empty() const			{ return begin == _end; };
 
-		/// @brief 当size超过capacity时翻倍，模拟STL
+		/// @brief 当size超过_capacity时翻倍，模拟STL
 		void extend();
 	};
 
@@ -48,7 +48,7 @@ namespace MercedesKK
 	void Queue<T>::push(const value_type& e)
 	{
 		extend();
-		data[end++] = e;
+		_data[_end++] = e;
 	}
 
 	template <typename T>
@@ -64,18 +64,18 @@ namespace MercedesKK
 	void Queue<T>::extend()
 	{
 		size_t sz = size();
-		if (sz == capacity - 1)
+		if (sz == _capacity - 1)
 		{
-			value_type* tmp = new value_type[capacity << 1];
+			value_type* tmp = new value_type[_capacity << 1];
 
-			for (int i = begin, j = 0; i != end; i++, j++)
-				tmp[j] = data[i];
+			for (int i = begin, j = 0; i != _end; i++, j++)
+				tmp[j] = _data[i];
 
 			begin = 0;
-			end = sz;
-			delete[]data;
-			data = tmp;
-			capacity <<= 1;
+			_end = sz;
+			delete[]_data;
+			_data = tmp;
+			_capacity <<= 1;
 		}
 	}
 }
